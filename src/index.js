1367ecbd3a09e05ab5953acdf8430e13198d3b9f
vera <nicoledallas16@gmail.com>
2024-01-30 19:48:32 -0500

    Added search functionality using the API

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
-function handleSearchSubmit(event) {
-    event.preventDefault();
-   let searchInput = document.querySelector(".search-form-input");
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+ cityElement.innerHTML = response.data.city;
+ temperatureElement.innerHTML = Math.round(temperature);
+
+}
+
+function searchCity(city) {
+  let apiKey = "013c2b91f3a4o14fc7369c10t4e1ea91";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiURL).then(refreshWeather);
+}
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector(".search-form-input");
+  
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
-
