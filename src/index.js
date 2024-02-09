fea0376d4e56d5c27cc96f32c1114d8543a451ab
vera <nicoledallas16@gmail.com>
2024-02-09 18:36:13 -0500

    injected more weather data

--- src/index.js
+++ src/index.js
@@ -2,11 +2,40 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windspeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+
+  
 
  cityElement.innerHTML = response.data.city;
+ timeElement.innerHTML = formatDate(date);
+ descriptionElement.innerHTML = response.data.condition.description;
+ humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+ windspeedElement.innerHTML = `${response.data.wind.speed}km/h`;
  temperatureElement.innerHTML = Math.round(temperature);
 
+
 }
+
+function formatDate(date) {
+  
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let day = days[date.getDay()];
+
+
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+
+  return `${day} ${hours}:${minutes}`;
+}
+
 
 function searchCity(city) {
   let apiKey = "013c2b91f3a4o14fc7369c10t4e1ea91";
