7723298350979adb2f5b41b525541073028ab452
vera <nicoledallas16@gmail.com>
2024-03-13 18:54:13 -0400

    Added forecast html from javascript

--- src/index.js
+++ src/index.js
@@ -53,5 +53,37 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+  function displayForecast() {
+    let days = ["Tue", "Wed", "Thur", "Fri", "Sat"];
+    let forecastHTML = ""; 
+
+    days.forEach(function (day) {
+      forecastHTML =
+        forecastHTML + 
+        `
+          <div class="weather-forecast-day">
+            <div class="weather-forecast-date">${day}</div>
+            <div class="weather-forecast-icon">🌤️</div>
+            <div class="weather-forecast-temperatures">
+              <div class="weather-forecast-temperature">
+                <strong>15º</strong>
+                <span>9º</span>
+              </div>
+            </div>
+          </div>
+        `;
+    });
+
+    let forecastElement = document.querySelector("#forecast");
+    forecastElement.innerHTML = forecastHTML; // Fixed variable name (from forecast to forecastHTML)
+  }
+
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris");
+displayForecast();
+
+
+
